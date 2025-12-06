0b533f0f1563d64b99c67bae30c685efd969bf8b
Diyaa2662 <diyaaaltaweel2002@gmail.com>
2025-12-06 21:46:31 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind!</h1>
+    </div>
+  );
+}
