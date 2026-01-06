99db748aadcddfebd2d9155d122fa3d886e458f8
Diyaa2662 <diyaaaltaweel2002@gmail.com>
2026-01-06 21:49:39 +0300

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // ⬅️ تأكد من وجود هذا السطر
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          devextreme: ['devextreme', 'devextreme-react'],
-        }
-      }
-    }
-  }
 })
